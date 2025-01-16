9a19a8469f39b9ded0a986df2ba13389fae3622d
Shreyoshi Das <shreyoshi4119@gmail.com>
2025-01-16 20:26:05 +0530

    setting up Header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-red-900"> do app</h1>
+      <Header></Header>
     </>
   );
 }
