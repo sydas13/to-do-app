691434b78345a7cac538b1d7d26c3c6adfcd26c8
Shreyoshi Das <shreyoshi4119@gmail.com>
2025-01-16 22:44:26 +0530

    changed Form component to Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
-import Form from "./components/Form";
+import Main from "./components/Main";
 function App() {
   return (
     <>
       <Header></Header>
-      <Form></Form>
+      <Main></Main>
     </>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <form>
+      <label htmlFor="todo-name"></label>
+      <input type="text" id="todo-name"></input>
+    </form>
+  );
+}
