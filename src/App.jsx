c44d3adff24b9d3db42b0161b07a173f7781e3fa
Shreyoshi Das <shreyoshi4119@gmail.com>
2025-01-16 20:23:37 +0530

    still testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-900"> do app</h1>
+      <h1 className="text-4xl font-bold text-red-900"> do app</h1>
     </>
   );
 }
