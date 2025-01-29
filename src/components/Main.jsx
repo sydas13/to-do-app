e2151299b55c4ea85ae6fde9d5968baac6d886c8
Shreyoshi Das <shreyoshi4119@gmail.com>
2025-01-29 12:28:37 +0530

    creating the filter dropdown function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,75 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Form from "./Form";
 import ToDo from "./ToDo";
+
 export default function Main() {
   const [todos, setTodos] = useState([]);
+  const [filterValue, setFilterValue] = useState("default");
 
-  const TodoContainer = todos.map((todo) => {
-    return <ToDo key={todo.taskCreated} {...todo} />;
-  });
+  let todoContainer = [];
+
+  const todoCompleteHtml = `<h1>task completed</h1>`;
+
+  const createTodoContainer = function (todos, filterValue) {
+    if (filterValue === "default") {
+      todoContainer = todos.map(
+        (todo) =>
+          !todo.isCompleted && (
+            <ToDo
+              key={todo.id}
+              {...todo}
+              handleComplete={handleComplete}
+            ></ToDo>
+          )
+      );
+    } else if (filterValue === "completed") {
+      todoContainer = todos.map(
+        (todo) =>
+          todo.isCompleted && (
+            <ToDo
+              key={todo.id}
+              {...todo}
+              handleComplete={handleComplete}
+            ></ToDo>
+          )
+      );
+    } else {
+      todoContainer = todos.map(
+        (todo) =>
+          !todo.isCompleted &&
+          todo.taskCategory === filterValue && (
+            <ToDo
+              key={todo.id}
+              {...todo}
+              handleComplete={handleComplete}
+            ></ToDo>
+          )
+      );
+    }
+  };
+
+  const removeToDo = function (id) {
+    setTodos((prev) => {
+      return prev.filter((todo) => todo.id != id);
+    });
+  };
+
+  const handleComplete = function (event) {
+    const todoId = event.currentTarget.id;
+    todos.forEach((todo) =>
+      todo.id === todoId ? { ...todo, isCompleted: true } : todo
+    );
+    const todoParent = event.currentTarget.closest(".todoContainer");
+    todoParent.innerHTML = todoCompleteHtml;
+
+    setTimeout(() => {
+      removeToDo(todoId);
+    }, 2000);
+  };
 
   const handleSubmit = function (event) {
     event.preventDefault();
-    console.log(event.currentTarget);
     const formData = new FormData(event.currentTarget);
     const taskName = formData.get("todo-name");
     const taskDescription = formData.get("todo-description");
@@ -26,17 +85,38 @@ export default function Main() {
       taskDueDate,
       taskDueTime,
       taskCategory,
-      taskCreated: new Date(),
+      taskCreated: new Date().getTime(),
+      id: uuidv4(),
     };
+
     setTodos((prev) => [...prev, todo]);
 
     event.currentTarget.reset();
   };
 
+  createTodoContainer(todos, filterValue);
+
+  const handleFilter = function (event) {
+    setFilterValue(event.currentTarget.value);
+    createTodoContainer(todos, filterValue);
+  };
+
   return (
     <main className="w-screen my-10 flex flex-row mx-auto justify-around">
       <Form handleSubmit={handleSubmit}></Form>
-      <div className="flex flex-col space-y-6 w-[50%]  ">{TodoContainer}</div>
+      <div className="flex flex-col space-y-6 w-[50%]  ">
+        <select
+          className="border-2 border-black mx-auto max-w-[200px] rounded-md px-1 py-2 font-form"
+          onChange={handleFilter}
+        >
+          <option value="default">Default</option>
+          <option value="work">Work</option>
+          <option value="personal">Personal</option>
+          <option value="shopping">Shopping</option>
+          <option value="completed">Completed</option>
+        </select>
+        {todoContainer}
+      </div>
     </main>
   );
 }
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,7 +1,5 @@
 import deleteBtn from "../assets/delete-btn.svg";
 export default function ToDos(props) {
-  console.log(props);
-
   const bgColor = {
     low: "bg-yellow-200",
     medium: "bg-red-300",
@@ -21,25 +19,29 @@ export default function ToDos(props) {
 
   return (
     <div
-      className={`${
+      className={`todoContainer ${
         bgColor[props.taskPriority]
-      } w-[90%]  max-w-[330px] sm:max-w-[500px]  rounded-md border-2 border-black hover:border-slate-500 mx-auto font-form px-3 pt-2 pb-3 flex flex-col space-y-2 relative`}
+      } w-[90%]  max-w-[330px] sm:max-w-[500px]  rounded-md border-2 border-black hover:border-slate-500 mx-auto font-form px-3 pt-2 pb-3 relative`}
     >
-      <h1 className="text-xl font-medium">{props.taskName.toUpperCase()}</h1>
-      <p className="break-words">{props.taskDescription}</p>
-      <p className="text-sm">
-        {formatDateTime(props.taskDueDate, props.taskDueTime)}
-      </p>
-      <input
-        type="checkbox"
-        name=""
-        className="absolute bottom-2 right-4 w-4 h-4 cursor-pointer"
-      ></input>
-      <img
-        src={deleteBtn}
-        aria-placeholder="delete-btn"
-        className="w-4 h-4 absolute bottom-2 right-11 cursor-pointer"
-      ></img>
+      <div className="flex flex-col space-y-2 ">
+        <h1 className="text-xl font-medium">{props.taskName.toUpperCase()}</h1>
+        <p className="break-words">{props.taskDescription}</p>
+        <p className="text-sm">
+          {formatDateTime(props.taskDueDate, props.taskDueTime)}
+        </p>
+        <input
+          onClick={props.handleComplete}
+          type="checkbox"
+          name=""
+          className="absolute bottom-2 right-4 w-4 h-4 cursor-pointer"
+          id={props.id}
+        ></input>
+        <img
+          src={deleteBtn}
+          aria-placeholder="delete-btn"
+          className="w-4 h-4 absolute bottom-2 right-11 cursor-pointer"
+        ></img>
+      </div>
     </div>
   );
 }
